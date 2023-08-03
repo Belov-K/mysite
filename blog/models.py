34581046c8869f7467532f78e762c37c2bab963e
kiril <kaz1nak3@mail.ru>
2023-08-03 12:16:37 +0300

    feat(blog): добавил канонические урл адреса

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 class Post(models.Model):
+    """
+    Пост
+    """
     STATUC_CHOICES=(
         ('draft', 'Draft'),
         ('publishe', 'Published'),
@@ -18,10 +22,15 @@ class Post(models.Model):
 
     class Meta:
         ordering=('-publish',)
+        verbose_name='Пост'
+        verbose_name_plural='Посты'
 
     def __str__(self):
         return self.title
 
-    class Meta:
-        verbose_name='Пост'
-        verbose_name_plural='Посты'
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.strftime('%m'),
+                             self.publish.strftime('%d'),
+                             self.slug])
